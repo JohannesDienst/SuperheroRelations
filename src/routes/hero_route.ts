b01430cf49e8bb3a232cb89d745453a0fdba1838
JohannesDienst <johannesdienst@t-online.de>
2017-01-29 21:24:30 +0100

    Routes, JSON-Database, Middleware

---
+++ src/routes/hero_route.ts
@@ -0,0 +1,102 @@
+/// <reference path="../hero_db.ts" />
+
+import { NextFunction, Request, Response, Router } from "express";
+import { BaseRoute } from "./base_route";
+
+const data: HeroDB = require("../../hero_db.json");
+
+export class HeroRoute extends BaseRoute {
+
+  public static create(router: Router) {
+    console.log("[HeroRoute::create] Creating Hero route.");
+
+    /*
+    getClient(1)
+    createClient(1)
+    updateAccountBalance(1)
+    addProductToOrder(1)
+    deleteAddress(1)
+    By contrast, the RESTful approach is to use:
+
+    GET /clients/1
+    POST /clients
+    PATCH /accounts/1
+    PUT /orders/1
+    DELETE /addresses/1
+    */
+    let routes = new HeroRoute();
+
+    router.get("/heros", routes.list);
+    router.get('/heros/:id', [routes.checkHeroExists, routes.find]);
+    router.post('/heros',
+      [routes.validateId, routes.validateName, routes.checkHeroExists, routes.post]
+    );
+    router.patch('/heros/:id', [routes.checkHeroExists, routes.patch]);
+    router.delete('/heros/:id', [routes.checkHeroExists, routes.delete]);
+  }
+
+  constructor() {
+    super();
+  }
+
+  private validateId(req: Request, res: Response, next) {
+    let id = parseInt(req.params.id, 10);
+    
+    if (isNaN(id)) {
+      next();
+    } else {
+      res.status(404).send('Invalid id');
+    }
+  }
+
+  private validateName(req: Request, res: Response, next) {
+    let name = req.params.name;
+
+    let re = new RegExp("^([a-zA-Z]{2,30})$");
+    if (re.test(name)) {
+      next();
+    } else {
+      res.status(404).send('Invalid id');
+    }
+  }
+
+  private checkHeroExists(req: Request, res: Response, next) {
+    let id = parseInt(req.params.id, 10);
+    
+    let hero = data.heros.filter(
+      function f(h: Hero) { return h.id === id; }
+    );
+
+    if (hero.length > 0) {
+      req['hero'] = hero[0];
+      next();
+    } else {
+      res.status(404).send('Invalid hero ID');
+    }
+  }
+
+  public list(req: Request, res: Response) {
+    console.log("list");
+    res.status(200).json(data);
+  }
+
+  public find(req: Request, res: Response) {
+    console.log("find");
+    res.status(200).json(req["hero"]);
+  }
+
+  public post(req: Request, res: Response) {
+    console.log("post");
+    res.json(req["hero"]);
+  }
+
+  public patch(req: Request, res: Response) {
+    console.log("patch");
+    res.json(req["hero"]);
+  }
+
+  public delete(req: Request, res: Response) {
+    console.log("delete");
+    res.json(req["hero"]);
+  }
+}
