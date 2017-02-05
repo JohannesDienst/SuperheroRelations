27461148cd7e61c2505df1eec24843d1c0449158
JohannesDienst <johannesdienst@t-online.de>
2017-02-05 21:34:42 +0100

    First version of promised based api call

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+/// <reference path="./hero_db.ts" />
+
+import Promise from "ts-promise";
+
+function getAsync<T>(): Promise<T>
+{
+  return Promise.resolve(
+    $.getJSON("http://localhost:8089/heros")
+  );
+}
+
+// TODO async/await version
 $( document ).ready(function() {
-    console.log( "ready!" );
+    console.log("Document ready");
+
+    let promise = getAsync<HeroDB>();
+    promise.then((data) => console.log(data));
 });
