79adfd133dd84814f655bf2370299ef3539ca46b
jdienst@multamedio.de <johannesdienst@t-online.de>
2017-02-06 14:41:21 +0100

    Raw version of async/await and append to table

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 /// <reference path="./hero_db.ts" />
 
-import Promise from "ts-promise";
-
 function getAsync<T>(): Promise<T>
 {
   return Promise.resolve(
@@ -9,10 +7,36 @@ function getAsync<T>(): Promise<T>
   );
 }
 
-// TODO async/await version
+async function getAsyncExp() {
+  let val = await getAsync<HeroDB>();
+  appendHeros(val);
+  return val;
+}
+
+function appendHeros(heros: HeroDB) {
+  let tableBody = $(".table tbody");
+  tableBody.find("tr").remove();
+  heros.heros.forEach(
+    (hero) => {
+      tableBody.append(
+        `<tr><td>${hero.id}</td><td>${hero.name}</td></tr>`
+      )
+    } 
+  );
+}
+
 $( document ).ready(function() {
     console.log("Document ready");
 
-    let promise = getAsync<HeroDB>();
-    promise.then((data) => console.log(data));
+    $("#get_async").on("click",
+      () => {
+        let promise = getAsync<HeroDB>();
+        promise.then(appendHeros);
+      }
+    );
+    $("#async_await").on("click", 
+      () => {
+        getAsyncExp();
+      }
+    );
 });
