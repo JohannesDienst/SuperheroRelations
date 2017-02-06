8bddb8a832ec05f4665095ff70f05aab1c3c32b3
jdienst@multamedio.de <johannesdienst@t-online.de>
2017-02-06 08:25:47 +0100

    Starting point for frontend async calls

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+
+
+function getAsync() {
+  console.log("getAsync");
+}
+
+async function getAsyncExp() {
+  console.log("getAsyncExp");
+}
+
 $( document ).ready(function() {
-    console.log( "ready!" );
+    console.log("ready!");
+
+    $("get_async").on("click", getAsync);
+    $("async_await").on("click", getAsyncExp);
 });
